c31017c516b55484da7203cf5fbc9ccdb1296e8f
yuval maymon <maymonyu@post.bgu.ac.il>
2018-05-20 17:43:48 +0300

    read data

--- ex_2.py
+++ ex_2.py
@@ -1,8 +1,18 @@
 import numpy as np
 
-enc1 = np.loadtxt("enc1.txt", delimiter=' ',  dtype=np.str)
-enc2 = np.loadtxt("enc2.txt", delimiter=' ',  dtype=np.str)
-dict = np.loadtxt("dict.txt", delimiter=' ',  dtype=np.str)
+def flatten_enc(enc):
+    enc = [line.split(" ") for line in enc]
+    enc = [item for sublist in enc for item in sublist]
+    return enc
+
+enc1 = np.loadtxt("enc1.txt", delimiter='\n',  dtype=np.str)
+enc1 = flatten_enc(enc1)
+
+enc2 = np.loadtxt("enc2.txt", delimiter='\n',  dtype=np.str)
+enc2 = flatten_enc(enc2)
+
+dict = np.loadtxt("dict.txt", delimiter='\n',  dtype=np.str)
+
 
 print enc1
 # print enc2
