40dfdc271576653419032a26f37ee5a7a67f4826
nofarmenashe <nofar1407@gmail.com>
2018-05-20 16:30:02 +0300

    add python file

---
+++ ex_2.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+enc1 = np.loadtxt("enc1.txt", delimiter=' ',  dtype=np.str)
+enc2 = np.loadtxt("enc2.txt", delimiter=' ',  dtype=np.str)
+dict = np.loadtxt("dict.txt", delimiter=' ',  dtype=np.str)
+
+print enc1
+# print enc2
+# print dict
