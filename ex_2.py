43b78df96ca0d1416eef5485c426e9b7b07496fc
nofarmenashe <nofar1407@gmail.com>
2018-05-20 17:41:28 +0300

    fix file loading - split to array of words

--- ex_2.py
+++ ex_2.py
@@ -1,9 +1,15 @@
+import json
 import numpy as np
 
-enc1 = np.loadtxt("enc1.txt", delimiter=' ',  dtype=np.str)
-enc2 = np.loadtxt("enc2.txt", delimiter=' ',  dtype=np.str)
-dict = np.loadtxt("dict.txt", delimiter=' ',  dtype=np.str)
 
-print enc1
-# print enc2
-# print dict
+def read_text_file(text_file_name):
+    with open(text_file_name, 'r') as file:
+        file_arr = []
+        for line in file.read().splitlines():
+            words_arr = [line.strip() for line in line.split(' ') if line.strip()]
+            file_arr.extend(words_arr)
+        return np.array(file_arr)
+
+enc1 = read_text_file("enc1.txt")
+enc2 = read_text_file("enc2.txt")
+dict = np.loadtxt("dict.txt", dtype=np.str)
